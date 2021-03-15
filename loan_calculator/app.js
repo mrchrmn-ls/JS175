1644a0adc6263dd880ff22d3689cfcf0a82e3bfd
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-03-15 13:45:46 +0100

    switched to POST

--- loan_calculator/app.js
+++ loan_calculator/app.js
@@ -2,9 +2,10 @@
 /* eslint-disable max-lines-per-function */
 const HTTP = require("http");
 const URL = require("url").URL;
-const HANDLEBARS = require("handlebars");
-const PATH = require("path");
+const QUERYSTRING = require('querystring');
 const FS = require("fs");
+const PATH = require("path");
+const HANDLEBARS = require("handlebars");
 
 const MIME_TYPES = {
   ".css": "text/css",
@@ -75,7 +76,7 @@ const LOAN_FORM_HTML = `
   <body>
     <article>
       <h1>Loan Calculator</h1>
-      <form action="/loan-offer" method="get">
+      <form action="/loan-offer" method="post">
         <p>All loans are offered at an APR of {{apr}}%.</p>
         <label for="amount">How much do you want to borrow (in dollars)?</label>
         <input type="number" name="amount" value="">
@@ -95,14 +96,34 @@ function render(template, data) {
   return html;
 }
 
-function getParams(path) {
-  const url = new URL(path, `http://localhost:${PORT}`);
-  return url.searchParams;
-}
-
 function getPathname(path) {
   const url = new URL(path, `http://localhost:${PORT}`);
   return url.pathname;
+}
+
+function parseFormData(request, callback) {
+  let body = "";
+    request.on("data", chunk => {
+      body += chunk.toString();
+    });
+    request.on("end", () => {
+      let data = QUERYSTRING.parse(body);
+      data.amount = Number(data.amount);
+      data.duration = Number(data.duration);
+      callback(data);
+    });
+}
+
+function getQueryData(path) {
+  const url = new URL(path, `http://localhost:${PORT}`);
+  let params = url.searchParams;
+  let amount = Number(params.get("amount"));
+  let duration = Number(params.get("duration"));
+
+  return {
+    amount,
+    duration
+  };
 }
 
 function monthlyPayment(amount, apr, years) {
@@ -114,22 +135,20 @@ function monthlyPayment(amount, apr, years) {
          .toFixed(2);
 }
 
-function loanOffer(params) {
-  let amount = Number(params.get("amount"));
-  let duration = Number(params.get("duration"));
+function loanOffer(data) {
 
-  return {
-    amount,
-    amountIncrement: amount + 100,
-    amountDecrement: amount - 100,
-    duration,
-    durationIncrement: duration + 1,
-    durationDecrement: duration - 1,
-    payment: monthlyPayment(amount, APR, duration)
-  };
+  return Object.assign(data, {
+    amountIncrement: data.amount + 100,
+    amountDecrement: data.amount - 100,
+    durationIncrement: data.duration + 1,
+    durationDecrement: data.duration - 1,
+    apr: APR,
+    payment: monthlyPayment(data.amount, APR, data.duration)
+  });
 }
 
 const SERVER = HTTP.createServer((req, res) => {
+  let method = req.method;
   let path = req.url;
   let pathname = getPathname(path);
   let fileExtension = PATH.extname(pathname);
@@ -141,20 +160,33 @@ const SERVER = HTTP.createServer((req, res) => {
       res.write(`${data}\n`);
       res.end();
 
-    } else if (pathname === "/") {
+    } else if (method === "GET" && pathname === "/") {
       let body = render(LOAN_FORM_TEMPLATE, {apr: APR});
+
       res.statusCode = 200;
       res.setHeader("Content-Type", "text/html; charset=UTF-8");
-      res.write(body);
+      res.write(`${body}\n`);
       res.end();
 
-    } else if (pathname === "/loan-offer") {
-      let loanData = loanOffer(getParams(path));
+    } else if (method === "GET" && pathname === "/loan-offer") {
+      let loanData = loanOffer(getQueryData(path));
       let body = render(LOAN_OFFER_TEMPLATE, loanData);
+
       res.statusCode = 200;
       res.setHeader("Content-Type", "text/html; charset=UTF-8");
-      res.write(body);
+      res.write(`${body}\n`);
       res.end();
+
+    } else if (method === "POST" && pathname === "/loan-offer") {
+      parseFormData(req, parsedData => {
+        let loanData = loanOffer(parsedData);
+        let body = render(LOAN_OFFER_TEMPLATE, loanData);
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`${body}\n`);
+        res.end();
+      });
 
     } else {
       res.statusCode = 404;
