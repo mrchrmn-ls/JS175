fe2498038e8501daac5fee8e3e6f93a586307086
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-03-19 09:57:02 +0100

    adding sessions and flash messages

--- projects/contacts/contact.js
+++ projects/contacts/contact.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const { body, validationResult } = require("express-validator");
-
+const session = require("express-session");
+const store = require("connect-loki");
+const flash = require("express-flash");
 
 const app = express();
+const LokiStore = store(session);
 
-let contactData = [
+const contactData = [
   {
     firstName: "Mike",
     lastName: "Jones",
@@ -44,6 +47,10 @@ function sortContacts(contacts) {
   });
 }
 
+function clone(object) {
+  return JSON.parse(JSON.stringify(object));
+}
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
@@ -52,13 +59,42 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan("common"));
 
+app.use(session({
+  cookie: {
+    httpOnly: true,
+    maxAge: 31 * 24 * 3600000,
+    path: "/",
+    secure: false
+  },
+  name: "launch-school-contacts-manager-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "this is not very secure",
+  store: new LokiStore({})
+}));
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  if (!("contactData" in req.session)) {
+    req.session.contactData = clone(contactData);
+  }
+  next();
+});
+
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+})
+
 app.get("/", (req, res) => {
   res.redirect("/contacts");
 });
 
 app.get("/contacts", (req, res) => {
   res.render("contacts", {
-    contacts: sortContacts(contactData)
+    contacts: sortContacts(req.session.contactData)
   });
 });
 
@@ -95,9 +131,9 @@ app.post("/contacts/new",
   (req, res, next) => {
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
-      console.log("errors");
+      errors.array().forEach(error => req.flash("error", error.msg));
       res.render("new-contact", {
-        errorMessages: errors.array().map(error => error.msg),
+        flash: req.flash(),
         ...req.body
       });
     } else {
@@ -106,7 +142,8 @@ app.post("/contacts/new",
   },
 
   (req, res) => {
-    contactData.push({ ...req.body });
+    req.session.contactData.push({ ...req.body });
+    req.flash("success", "New contact added to the database.");
     res.redirect("/contacts");
   }
 );
